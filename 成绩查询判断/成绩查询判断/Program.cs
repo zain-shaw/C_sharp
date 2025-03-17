d97c3aa8b28d4b3b34aaad0e940cbaa7fb66183d
Zain-Shaw <xiaozekai2022@163.com>
2025-03-17 21:06:34 +0800

    3.17

---
+++ 成绩查询判断/成绩查询判断/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 成绩查询判断
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool B_a = true;
+
+            while(B_a)
+            {
+                Console.WriteLine("请输入你的成绩。");
+                string S_score = Console.ReadLine();
+                Console.WriteLine("请问你是A同学还是B同学。（请输入A或B）");
+                string S_x = Console.ReadLine();
+                int score = int.Parse(S_score);
+
+
+                switch (S_x)
+                {
+                    case "A":
+                        Console.WriteLine("你好，A同学。");
+                        break;
+                    case "B":
+                        Console.WriteLine("你好，B同学。");
+                        break;
+                }
+                
+
+
+                if (score >= 80)
+                {
+                    Console.WriteLine("恭喜,好厉害的分数，成绩为：" + score);
+                }
+                else if (score >= 60 && score < 80)
+                {
+                    Console.WriteLine("分数不高，仍需努力，成绩为：" + score);
+                }
+                else
+                {
+                    Console.WriteLine("遗憾，下次努力。成绩为：" + score);
+                }
+
+
+
+                Console.WriteLine("是否结束程序，请输入YES或NO。");
+                string input = Console.ReadLine();
+                if (input.ToUpper() == "YES")
+                {
+                    B_a = false;
+                    Console.WriteLine("程序结束。");
+                    break;
+                }
+                else if (input =="shaw_zain")
+                {
+                    B_a = true;
+                    Console.WriteLine("恭喜发现彩蛋！！！");
+                }
+                else
+                {
+                
+                    B_a = true;
+                    Console.WriteLine("程序继续");
+                }
+
+
+            }
+
+
+         
+                Console.WriteLine("while循环");
+                int sum = 0, I_i = 1;
+                while (I_i <= 100)
+                {
+                    sum += I_i;
+                    I_i++;
+                    Console.WriteLine("第" + I_i + "循环");
+                }
+            Console.WriteLine("最终值为" + sum);
+
+
+            Console.WriteLine("DO/WHILE循环");
+            do
+            {
+            sum += I_i;
+            I_i++;
+            Console.WriteLine("第" + I_i + "循环");
+            } while (I_i <= 100);
+            Console.WriteLine("最终值为" + sum);
+
+            
+
+
+            Console.WriteLine("IF循环");
+            for (int I_a = 1; I_a <= 100; I_a++)
+            {
+                sum += 1;
+                Console.WriteLine("第" + I_a + "循环");
+            }
+            Console.WriteLine("最终值为" + sum);
+
+            {
+                Console.WriteLine("数组计算1");
+                int I_b = 0;
+                int[] I_project1 = { 1, 2, 3, 4, 5 };
+                foreach (int number in I_project1)
+                {
+                    I_b += number;
+                }
+                Console.WriteLine("最终值为" + I_b);
+            }
+            {
+                Console.WriteLine("数组计算2,break");
+                int I_b = 0;
+                int[] I_project1 = { 1, 2, 3, 4, 5 };
+                foreach (int number in I_project1)
+                {
+                    if(number<2)
+                    {
+                        Console.WriteLine("跳出。");
+                        break;
+                    }
+                    I_b += number;
+                }
+                Console.WriteLine("最终值为" + I_b);
+            }
+            {
+                Console.WriteLine("数组计算3,continue,");
+                int I_b = 0;
+                int[] I_project1 = { 1, 2, 3, 4, 5 };
+                foreach (int number in I_project1)
+                {
+                    if (number < 2)
+                    {
+                        continue;
+                    }
+                    I_b += number;
+                }
+                Console.WriteLine("最终值为" + I_b);
+            }
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 窗体1/窗体1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace 窗体1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "系统1";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("隶书", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.Aqua;
+            this.button1.Location = new System.Drawing.Point(81, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "点击跳转";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
