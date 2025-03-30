5d3eb71634a4d710dbd2a93c88af4ce0893e00da
Zain-Shaw <xiaozekai2022@163.com>
2025-03-30 17:47:51 +0800

    3.30学习

---
+++ 2.4.3_项目三求解斐波那契数列/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace _2._4._3_项目三
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("版本一，for_if版：\n  ");
+            int Ia=0, Ib=1,input=0;
+            for(int time=1;time<=20; time++)
+            {
+                if(time==1)
+                {
+                    input = Ia;
+                    Console.WriteLine("数字"+time+":"+input + "\n");
+                }
+                else if(time==2)
+                {
+                    input = Ib;
+                    Console.WriteLine("数字"+time + ":" + input + "\n");
+                }
+                else
+                {
+                    input = Ia + Ib;
+                    Console.WriteLine("数字"+time + ":" + input + "\n");
+                    Ia = Ib;
+                    Ib = input;
+                }
+               
+            }
+
+
+            Console.WriteLine("版本二，数组版：\n");
+            int[] series = new int[20];
+            series[0] = 0;
+            Console.WriteLine("数字1:" + series[0]);
+            series[1] = 1;
+            Console.WriteLine("数字2:" + series[1]);
+            for (int time = 2; time < series.Length; time++)
+            {
+                series[time] = series[time - 1] + series[time - 2];
+                Console.WriteLine("数字"+time+":"+series[time]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2.4.4_项目四：数字古诗的保存和输出/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace _2._4._4_项目四_数字古诗的保存和输出
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] sentence1 = new char[] { '归', '来', '一', '只', '复', '一', '只' };
+            char[] sentence2 = new char[] { '三', '四', '五', '六', '七', '八', '只' };
+            char[] sentence3 = new char[] { '凤', '凰', '何', '少', '鸟', '何', '多' };
+            char[] sentence4 = new char[] { '啄', '尽', '人', '间', '千', '石', '食' };
+            Console.WriteLine("横向展示;\n");
+            {
+                for(int time=1;time<=4;time++)
+                {
+                    if(time==1)
+                    {
+                        for(int word=0;word<7;word++)
+                        {
+                            Console.Write(sentence1[word]);
+                            if(word==6)
+                            {
+                                Console.Write("\n");
+                            }
+                        }
+                    }//输出一句
+
+
+                    if (time == 2)
+                    {
+                        for (int word = 0; word < 7; word++)
+                        {
+                            Console.Write(sentence2[word]);
+                            if (word == 6)
+                            {
+                                Console.Write("\n");
+                            }
+                        }
+                    }//输出一句
+
+
+                    if (time == 3)
+                    {
+                        for (int word = 0; word <7; word++)
+                        {
+                            Console.Write(sentence3[word]);
+                            if (word == 6)
+                            {
+                                Console.Write("\n");
+                            }
+                        }
+                    }//输出一句
+
+
+                    if (time == 4)
+                    {
+                        for (int word = 0; word <7; word++)
+                        {
+                            Console.Write(sentence4[word]);
+                            if (word == 6)
+                            {
+                                Console.Write("\n");
+                            }
+                        }
+                    }//输出一句
+
+                }
+
+
+
+            }
+
+
+            Console.WriteLine("\n纵向展示;\n");
+            {
+                for (int time = 0; time <=7; time++)
+
+                {
+                    Console.Write(sentence1[time] + "\t");
+                    Console.Write(sentence2[time] + "\t");
+                    Console.Write(sentence3[time] + "\t");
+                    Console.Write(sentence4[time] + "\t");
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
